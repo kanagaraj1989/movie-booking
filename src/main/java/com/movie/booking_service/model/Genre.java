4147e3a7a6b1222b42c122ab3688b2ccef01777e
Kanagaraj K <k.kanagaraj@glofox.com>
2025-02-02 18:21:29 +0530

    refactor movie entity.

---
+++ src/main/java/com/movie/booking_service/model/Genre.java
@@ -0,0 +1,22 @@
+package com.movie.booking_service.model;
+
+public enum Genre {
+    ACTION,
+    ADVENTURE,
+    COMEDY,
+    CRIME,
+    DRAMA,
+    FANTASY,
+    HISTORICAL,
+    HORROR,
+    MYSTERY,
+    PHILOSOPHICAL,
+    POLITICAL,
+    ROMANCE,
+    SAGA,
+    SATIRE,
+    SCIENCE_FICTION,
+    THRILLER,
+    URBAN,
+    WESTERN
+}
--- src/main/java/com/movie/booking_service/model/Movie.java
+++ src/main/java/com/movie/booking_service/model/Movie.java
@@ -1,6 +1,7 @@
 package com.movie.booking_service.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -8,6 +9,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @Data
@@ -19,7 +21,11 @@ public class Movie {
     @Id
     private UUID id;
     private String name;
-    private String language;
+    private String summary;
+    private List<String> language;
+    private String originalLanguage;
+    @Enumerated
+    private Genre genre;
     private int duration;
     private LocalDateTime releaseDate;
 }
